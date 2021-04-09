5f31e583d38a42125ae43a2e029844c1bbfe3c4f
usma11pb <82115713+usma11pb@users.noreply.github.com>
2021-04-09 10:44:21 -0400

    Update Configuration.h

--- marlin_changes/Configuration.h
+++ marlin_changes/Configuration.h
@@ -1022,11 +1022,11 @@
 #define MIN_PROBE_EDGE  0
 
 // X and Y axis travel speed (mm/m) between probes
-#define XY_PROBE_SPEED  4000
+#define XY_PROBE_SPEED  400
 
 // Feedrate (mm/m) for the first approach when double-probing
 // (MULTIPLE_PROBING == 2)
-#define Z_PROBE_SPEED_FAST  (HOMING_FEEDRATE_Z / 5)
+#define Z_PROBE_SPEED_FAST  (HOMING_FEEDRATE_Z / 10)
 
 // Feedrate (mm/m) for the "accurate" probe of each point
 #define Z_PROBE_SPEED_SLOW  (Z_PROBE_SPEED_FAST / 3)
